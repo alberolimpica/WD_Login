23fc6ff8e713710faf287a10bbc1c1cf7300d942
Sara Alberola Mahiques <alberolimpica@users.noreply.github.com>
2021-11-01 13:48:14 +0100

    Security Level 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema =  new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -35,10 +33,11 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
